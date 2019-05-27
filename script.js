644869b01e659ff65220244125906bf03886d51d
Claud Pod <claudiapollinger.cp@gmail.com>
2019-05-26 19:28:09 -0600

    got the giphy on the buttons

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
 var topics = ["Swimming", "Hiking", "Mountains", "River", "Kayaking", "Camping", "Trails", "Disk Golf", "Paddle Boarding", "Fishing", "Skiing"];
-
-// for (var i = 0; i <topics.length; i++){
-//     console.log(i=);
-//     console.log(topics[i]);
-// }
-var queryURL = "https://api.giphy.com/v1/gifs/random" 
-      queryURL +="?api_key=KsHx01jSsGL9FW02U0n28y1XP2eCXcLd&tag=outdoors"
-
-
-      // rendering one button for each outdoor activity in the topic array 
+// rendering one button for each outdoor activity in the topic array 
 function renderButtons() {
     $("#outdoor-buttons").empty();
 
-    topics.forEach(function(topic) {
+    topics.forEach(function (topic) {
         // creating button
         var button = $("<button>");
         button.text(topic);
@@ -25,15 +16,65 @@ function renderButtons() {
 
 }
 
-$("#add-outdoor").on("click", function(event) {
+$("#add-outdoor").on("click", function (event) {
     event.preventDefault();
-    
-     
+
+
     var outdoorAct = $("#outdoor-input").val().trim();
     topics.push(outdoorAct);
     renderButtons();
     $("#outdoor-input").val("");
 });
 
-/// ~CALL THE FUNCTIONS~///
+$("add-outdoor").on("submit", function (event) {
+    event.preventDefault();
+    var topic = $("#outdoor-input").val().trim();
+    topics.push(topic);
+    renderButtons();
+    $("#outdoor-input").val("");
+});
+
+/// ~CALL THE render button FUNCTION~///
 renderButtons();
+
+// event delegation syntax 
+$("#outdoor-buttons").on("click", "button", function () {
+
+    var searchTerm = $(this).attr("data-title");
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search"
+    queryURL += "?api_key=KsHx01jSsGL9FW02U0n28y1XP2eCXcLd&q=" + searchTerm + "&limit=10";
+
+    console.log(queryURL);
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then (function (response) {
+            console.log(response)
+
+                for (var i = 0; i < response.data.length; i++) {
+                    var stillImage = response.data[i].images.fixed_width_still.url;
+                    var animatedImage = response.data[i].images.fixed_width.url;
+                    var mainURL = stillImage;
+                    var gifImage = $("<img>").attr("src", mainURL).attr("data-still", stillImage).attr("data-animate", animatedImage).attr("data-state", "still").addClass("gif");
+                    var rating = response.data[i].rating;
+                    var p = $("<p>").text("Rating: " + rating);
+                    $(".gifs").prepend(p, gifImage);
+                }
+                //stops and starts gifs with on.click event
+                $(".gif").on("click", function () {
+                    var state = $(this).attr("data-state");
+                    if (state === "still") {
+                        $(this).attr("src", $(this).attr("data-animate"));
+                        $(this).attr("data-state", "animate");
+                    } else {
+                        $(this).attr("src", $(this).attr("data-still"));
+                        $(this).attr("data-state", "still");
+
+            }
+      
+    });
+});
+
+});
